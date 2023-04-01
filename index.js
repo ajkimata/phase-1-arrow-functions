a3ef5cafe021b73444a31fa20bc43b08c0440acb
alfred kimata <alfyjason@gmail.com>
2023-04-01 19:02:03 +0300

    Arrow functions done

---
+++ index.js
@@ -0,0 +1,14 @@
+// Define the divide function
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Define the square arrow function
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Define the add arrow function
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
